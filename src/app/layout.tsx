d6d9a809a1006c855af6767901b79bb6e82ee031
Nate Bridi <nate.bridi@gmail.com>
2026-02-27 15:06:32 -0500

    fix type issue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link
           href="https://fonts.googleapis.com/css2?family=Trispace:wght@100..800&family=Playfair+Display:wght@700&family=Vollkorn:ital,wght@0,400;0,700;1,400;1,700&display=swap"
           rel="stylesheet"
